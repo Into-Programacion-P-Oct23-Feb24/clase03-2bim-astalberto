d655305d3b247000292c3ab1ba056b3fd1e385e7
astalberto <albertoherrera205@hotmail.com>
2023-12-20 15:48:57 -0500

    Segundo Push

--- Ejemplosc3/src/paquete01/Ejemplo02.java
+++ Ejemplosc3/src/paquete01/Ejemplo02.java
@@ -21,30 +21,31 @@ public class Ejemplo02 {
         // Creación de arreglo bidimensionales
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-
+        int valor;
         
         // forma 1
         int[][] arreglo1 = new int[3][4]; //
         System.out.printf("Uso de la opción length: %d\n", arreglo1.length);
-        
+
         System.out.printf("Valor en la posición [0][2]\n", arreglo1[0][2]);
-       
+
         arreglo1[0][2] = 1000;
-        
+
         for (int i = 0; i < arreglo1.length; i++) {
             for (int j = 0; j < arreglo1[i].length; j++) {
-                System.out.printf("Ingrese el valor de [%d][%d]\n",i,j);
-                arreglo1[i][j]= entrada.nextInt();
+                System.out.printf("Ingrese el valor de [%d][%d]\n", i, j);
+                valor = entrada.nextInt();
+                arreglo1[i][j] = valor;
             }
         }
-               
+
         for (int fila = 0; fila < arreglo1.length; fila++) {
             for (int col = 0; col < arreglo1[fila].length; col++) {
-                System.out.printf("fila[%d] columna[%d] = %d\n", 
+                System.out.printf("fila[%d] columna[%d] = %d\n",
                         fila, col, arreglo1[fila][col]);
             }
         }
-        
+
     }
-    
+
 }
