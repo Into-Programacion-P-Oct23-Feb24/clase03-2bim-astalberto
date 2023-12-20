8a0be66178354a3e2e45dabd36eb58d66900e67e
astalberto <albertoherrera205@hotmail.com>
2023-12-20 15:27:31 -0500

    Primer Push

--- Ejemplosc3/src/paquete05/Ejemplo001.java
+++ Ejemplosc3/src/paquete05/Ejemplo001.java
@@ -8,12 +8,12 @@ package paquete05;
  *
  * @author LAB.ELECT
  */
-public class ejemplo001 {
+public class Ejemplo001 {
 
     public static void main(String[] args) {
         int[] arreglo1 = new int[2];
         for (int i = 0; i < 2; i++) {
-            System.out.println("fila " + i +" = "+ arreglo1[i] + "\n");
+            System.out.println("fila " + i + " = " + arreglo1[i] + "\n");
         }
     }
 }
